1609f6afdeecdeac4ba5cc8e5c183dc62a96f196
m25shubhamkumar-ops <m25.shubham.kumar@msot-hiet.org>
2026-03-26 01:04:36 +0530

    Initial commit of Application.kt for Ktor server setup

---
+++ backend/src/main/kotlin/com/hackathon/Application.kt
@@ -0,0 +1,17 @@
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            gson {}
+        }
+        routing {
+            // Define your routes here
+        }
+    }.start(wait = true)
+}
